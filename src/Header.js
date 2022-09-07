3364188d101378ab80c34338b8b31be7e7e15d8e
alvinho2203 <andrezo10@hotmail.com>
2022-09-06 21:17:35 -0300

    Página faltando ajustes finos

--- src/Header.js
+++ src/Header.js
@@ -16,6 +16,8 @@ import baseConteudo3 from './imagem/baseConteudo3.png'
 import baseConteudo4 from './imagem/baseConteudo4.png'
 import baseConteudo5 from './imagem/baseConteudo5.png'
 import local from './imagem/local.png'
+import logoredonda from './imagem/logoredonda.png'
+import homem from './imagem/homem.png'
 
 function Header(props){
     return(
@@ -66,10 +68,10 @@ function Header(props){
                         <div className="formulario">
                             <h2 className="tituloForm">Lorem ipsum dolor sit amet?</h2>
                             <form action="submit">
-                                <input className="inputs" type="text" value="Nome" id="nome" />
-                                <input className="inputs" type="text" value="Seu site" id="seusite" />
-                                <input className="inputs" type="text" value="Email" id="email" />
-                                <input className="inputs" type="tel" value="Telefone" id="telefone" />
+                                <input className="inputs" type="text"  placeholder="Nome" id="nome" />
+                                <input className="inputs" type="text"  placeholder="Seu Site" id="seusite" />
+                                <input className="inputs" type="text"  placeholder="Email" id="email" />
+                                <input className="inputs" type="tel" placeholder="Telefone" id="telefone" />
                                 <br/>
                                 <br/>
                                 <button className="btnEnviar" type="submit" name="enviar" value="Submit">Enviar</button>
@@ -155,7 +157,149 @@ function Header(props){
                     </div>
                 </div>
                 <div className="Parte3">
-                    
+                    <div>
+                        <p className="subTitleParte3">Lorem ipsum</p>
+                        <p className="titleParte3">Lorem<br/> ipsum</p>
+                        <div className="subtitParte3Linha"></div>
+                        <p className="textParte3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div>
+                        <div className="pessoa1">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="pessoa2">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="pessoa3">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="pessoa4">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="pessoa5">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                        <div className="pessoa6">
+                            <div className="bola2"></div>
+                            <p className="tituloPessoa1">Lorem ipsum</p>
+                            <p className="textoPessoa1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button className="btnContato2">CONTATO</button>
+                    </div>
+                </div>
+                <div className="Parte4">
+                    <p className="tituloParte4">Lorem <b className="tituloGrandeParte4">Ipsun dolor</b> sit amet</p>
+                </div>
+                <div className="Parte5">
+                    <div>
+                        <img className="logoRedonda" src={logoredonda}/>
+                        <p className="tituloParte5">Lorem ipsum dolor</p>
+                        <p className="textoParte5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum porta arcu vel vulputate.</p>
+                    </div>
+                    <div>
+                        <div className="imgProduto"></div>
+                        <div>
+                            <p className="descProduto"><b>Lorem ipsum dolor:</b> sit amet, consectetur adipiscing</p>
+                            <p className="tituloProduto">Lorem ipsum</p>
+                            <p className="precoProduto">XXX.XXX</p>
+                            <p className="desc2Produto">Lorem ipsum</p>
+                            <div className="linhaProduto"></div>
+                            <p className="precoProduto2">XXX%</p>
+                            <p className="desc3Produto">Lorem ipsum</p>
+                            <p className="desc4Produto"><b>Lorem Ipsum:</b> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Parte6">
+                    <div>
+                        <img className="logoRedonda" src={logoredonda}/>
+                        <p className="tituloParte5">Lorem ipsum dolor</p>
+                        <p className="textoParte5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum porta arcu vel vulputate.</p>
+                    </div>
+                    <div>
+                        <div className="img2Produto"></div>
+                        <div>
+                            <p className="descProduto"><b>Lorem ipsum dolor:</b> sit amet, consectetur adipiscing</p>
+                            <p className="tituloProduto">Lorem ipsum</p>
+                            <p className="precoProduto">XXX.XXX</p>
+                            <p className="desc2Produto">Lorem ipsum</p>
+                            <div className="linhaProduto"></div>
+                            <p className="precoProduto2">XXX%</p>
+                            <p className="desc3Produto">Lorem ipsum</p>
+                            <p className="desc4Produto"><b>Lorem Ipsum:</b> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Parte7">
+                    <div>
+                        <img className="logoRedonda" src={logoredonda}/>
+                        <p className="tituloParte5">Lorem ipsum dolor</p>
+                        <p className="textoParte5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum porta arcu vel vulputate.</p>
+                    </div>
+                    <div>
+                        <div className="imgProduto"></div>
+                        <div>
+                            <p className="descProduto"><b>Lorem ipsum dolor:</b> sit amet, consectetur adipiscing</p>
+                            <p className="tituloProduto">Lorem ipsum</p>
+                            <p className="precoProduto">XXX.XXX</p>
+                            <p className="desc2Produto">Lorem ipsum</p>
+                            <div className="linhaProduto"></div>
+                            <p className="precoProduto2">XXX%</p>
+                            <p className="desc3Produto">Lorem ipsum</p>
+                            <p className="desc4Produto"><b>Lorem Ipsum:</b> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Parte8">
+                    <div>
+                        <img className="logoRedonda" src={logoredonda}/>
+                        <p className="tituloParte5">Lorem ipsum dolor</p>
+                        <p className="textoParte5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum porta arcu vel vulputate.</p>
+                    </div>
+                    <div>
+                        <div className="img2Produto"></div>
+                        <div>
+                            <p className="descProduto"><b>Lorem ipsum dolor:</b> sit amet, consectetur adipiscing</p>
+                            <p className="tituloProduto">Lorem ipsum</p>
+                            <p className="precoProduto">XXX.XXX</p>
+                            <p className="desc2Produto">Lorem ipsum</p>
+                            <div className="linhaProduto"></div>
+                            <p className="precoProduto2">XXX%</p>
+                            <p className="desc3Produto">Lorem ipsum</p>
+                            <p className="desc4Produto"><b>Lorem Ipsum:</b> dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="Parte9">
+                    <img className="homem" src={homem}/>
+                    <div className="tituloParte9">Lorem ipsum</div>
+                    <div className="subtituloParte9">Lorem ipsum</div>
+                    <div className="textoParte9">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis interdum porta arcu vel vulputate. Morbi quis egestas dui. Integer nibh mauris, pulvinar eu tempus eget, facilisis et nunc.</div>
+                    <div className="formularioParte9">
+                            <form action="submit">
+                                <input className="inputs1Parte9" type="text" value="Nome" id="nome" />
+                                <input className="inputs2Parte9" type="text" value="Seu site" id="seusite" />
+                                <input className="inputs3Parte9" type="text" value="Email" id="email" />
+                                <input className="inputs4Parte9" type="tel" value="Telefone" id="telefone" />
+                                <br/>
+                                <br/>
+                                <button className="btnEnviarParte9" type="submit" name="enviar" value="Submit">Enviar</button>
+                            </form>
+                    </div>
                 </div>
             </body>    
         </header>
